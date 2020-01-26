4bfb2cc3c308581fe12709a66be5f4da67d55ee6
iambillmccann <bill.mccann@gmail.com>
2020-01-25 21:03:10 -0500

    Main program user input loop

--- ProjectEuler/ProjectEuler.cpp
+++ ProjectEuler/ProjectEuler.cpp
@@ -2,19 +2,92 @@
 //
 
 #include <iostream>
+#include <string>
+#include <algorithm>
 
-int main()
-{
-    std::cout << "Hello World!\n";
+using namespace std;
+string kQuitString = "Q";
+const int kLastProblem = 0;
+
+int GetUserInput();
+
+// to_upper is a function to convert a string to all upper case.
+// This function is used in conjunction with for_each to convert
+// each individual character.
+//
+// Args:
+//    ch: The character to convert to upper case
+void to_upper(char& ch) {
+    ch = toupper(static_cast<unsigned char>(ch));
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
+// checkUserInput will validate the user's input. This is a very redimentary
+// validation method. It checks three things ...
+// 1. Checks for the letter "Q"
+// 2. Checks for an integer
+// 3. Checks that the integer is positive
+// 4. Checks that the integer is no greater than the last problem solved.
+// These checks assume that problems are solved in order.
+// Note: If the user enters "Q", a minus one is returned to the caller.
+//
+// Args:
+//     userInput: A string entered by the user.
+//
+// Returns:
+//     A valid integer result.
+int CheckUserInput(string user_input)
+{
+    int result = -1;
 
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+    for_each(user_input.begin(), user_input.end(), to_upper);
+    if (user_input.compare(kQuitString) == 0) return result;
+
+    try {
+        result = stoi(user_input);
+        if (result < 1)
+        {
+            cout << "\nBTW, problem numbers are positive integers.\n";
+            return GetUserInput();
+        }
+        else if (result > kLastProblem) {
+            cout << "\nI have only completed problems 1 through " << kLastProblem << "\n";
+            return GetUserInput();
+        }
+        return result;
+    }
+    catch (exception) {
+        cout << "\nSorry but I did not understand that. Please type the problem number or Q to quit.\n";
+        return GetUserInput();
+    }
+}
+
+// getUserInput is a simple method for reading from the console.
+//
+// Returns: 
+//     A string that represents the user's input.
+int GetUserInput()
+{
+    string user_input;
+
+    cout << "What problem shall I run? Or type 'Q' to quit. ";
+    getline(cin, user_input);
+    
+    return CheckUserInput(user_input);
+}
+
+//
+// This is the main program
+//
+int main()
+{
+    int problem_number = 0;
+
+    //ToDo: instantiate a factory to instantiate the problem solution
+    //ToDo: instantiate classes for the stop watch
+
+    problem_number = GetUserInput();
+    while (problem_number > 0) {
+        cout << "The problem number is " << problem_number;
+        problem_number = GetUserInput();
+    }
+}
